446e0d77ca32e2c04726e28cc96425f8dc1f8927
nyko <nickolasx50@gmail.com>
2021-01-18 15:27:13 -0300

    arreglos

--- request_nasa.rb
+++ request_nasa.rb
@@ -3,15 +3,15 @@ require "net/http"
 require "openssl"
 require "json"
 
-def request("http://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&api_key=DEMO_KEY")
+def request(url)
     url = URI("http://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&api_key=DEMO_KEY")
     http = Net::HTTP.new(url.host, url.port)
     http.use_ssl = true
     request = Net::HTTP::Get.new(url)
+    http.verify_mode = openssl::SSL::VERIFY_PEER
     request ["cache-control"] = 'no-cache'
-
     response = http.request(request)
-    body = JSON.parse response.read_body
-    body.each do |photos|
-        puts photos['']
+
+   return JSON.parse(response.read_body)
+
 end
